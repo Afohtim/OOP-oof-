f3d4437de78f283bf5324447f587e7d375ad689b
Afohtim <afohmouth@gmail.com>
2018-09-30 20:27:41 +0300

    Tree interface

--- Lab1/Lab1/Tree.h
+++ Lab1/Lab1/Tree.h
@@ -1,12 +1,27 @@
 #pragma once
+#include "List.h"
 
 
+template<typename TValue>
+struct TNode
+{
+	TValue val;
+	unsigned key;
+	List* childrens;
+};
 
 
-
+template<typename TValue>
 class Tree
 {
+private:
+	TNode<TValue>* root;
+	TNode<TValue>* dfs(unsigned key = 0);
+	unsigned nextkey
 public:
+	void insert(unsigned key, TValue value);
+	void erase(unsigned key);
+	TValue key(unsigned key);
 	Tree();
 	~Tree();
 };
