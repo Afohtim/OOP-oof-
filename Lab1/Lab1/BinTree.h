d72fd0fe7aea0548a17908bb64ab867e302149f5
Afohtim <afohmouth@gmail.com>
2018-10-09 22:53:55 +0300

    gitignore changes

--- Lab1/Lab1/BinTree.h
+++ Lab1/Lab1/BinTree.h
@@ -1,8 +1,23 @@
 #pragma once
+#include <stdlib.h> 
+
+struct BinNode
+{
+	BinNode* left;
+	BinNode* right;
+	int val;
+
+};
+
 class BinTree
 {
+private:
+	BinNode* root;
 public:
 	BinTree();
 	~BinTree();
+	void insert(int val);
+
+
 };
 
